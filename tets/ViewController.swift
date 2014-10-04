6a2b999b0f6a467943b109fb699ef1c5aead3914
crsmnd <crsmnd@gmail.com>
2014-10-04 13:46:48 -0300

    first rev of project

---
+++ tets/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  tets
+//
+//  Created by Vincent on 01/10/2014.
+//  Copyright (c) 2014 VD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+    @IBAction func sendMailButton(sender: UIButton) {
+        messageLabel.text = enterNameTextField.text
+        messageLabel.hidden = false
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        messageLabel.textColor = UIColor.redColor()
+        sendButton.setTitle("Mail Sent", forState: UIControlState.Highlighted)
+        
+        
+    }
+
+}
+
